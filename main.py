8ed791c5d08fc0be82534ea19db769e33f605d0f
thep200 <thepqlna@gmail.com>
2021-05-14 15:57:22 +0700

    export segmet audio

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import re
+import random
 import numpy as np
-from datetime import time
-import scipy.io as wavfile
-
-path = "datan/01.srt"
+import scipy.io.wavfile as wav
+from pydub import AudioSegment
+from python_speech_features import mfcc
+from python_speech_features import logfbank
 
 # Chuyển thời gian trong file srt sang milisecond
 def convert_to_time(time_start, time_end):
@@ -13,7 +14,7 @@ def convert_to_time(time_start, time_end):
     
 
 # Tách thời gian trong file srt và đoạn text tương ứng 
-def get_time_segment():
+def get_time_segment(path, word_means):
     time_segment = []
 
     with open(path, "r", encoding='utf-8') as file:
@@ -27,14 +28,29 @@ def get_time_segment():
             array_line[1] = array_line[1].replace(',', '.')
 
             # i là thời gian tương ứng i + 1 là đoạn text tương ứng với segment đó
-            time_segment.append(convert_to_time(array_line[0], array_line[1])) 
-            time_segment.append(lines[lines.index(line) + 1][0:-1])
+            if lines[lines.index(line) + 1][0:-1] == word_means:
+                time_segment.append(convert_to_time(array_line[0], array_line[1]))
+                # time_segment.append(lines[lines.index(line) + 1][0:-1])
 
     return time_segment
 
 
+def extract_audio_segment(path, time, name_audio):
+    newAudio = AudioSegment.from_wav(path)
+    newAudio = newAudio[time[0]:time[1]]
+    newAudio.export(name_audio + '.wav', format="wav") 
+
+
 if __name__ == '__main__':
-    my_segments = get_time_segment()
-    for i in range(0, len(my_segments), 2):
-        print(my_segments[i],'    ', my_segments[i + 1], '\n')
+    # list_path = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+    # for path in list_path:
+    #     list_segment_cho = get_time_segment('datan/' + path + '.srt', 'cho')
+    #     for _ in list_segment_cho:
+    #         extract_audio_segment('datan/' + path + '.wav', _, 'data_export/' + path + '_' + str(random.randint(0, 999999)))    
+
+    (rate, sig) = wav.read('data_export/01_925408.wav')
+    mfcc_feat = mfcc(sig,rate)
+    fbank_feat = logfbank(sig,rate)
+
+    print(fbank_feat[1:3, :])
 
