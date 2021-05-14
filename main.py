6fe8fb9c640776ccaf6f45ea8157cfa5295ef8e3
thep200 <thepqlna@gmail.com>
2021-05-14 16:16:48 +0700

    small changes

--- main.py
+++ main.py
@@ -31,23 +31,20 @@ def get_time_segment(path, word_means):
             if lines[lines.index(line) + 1][0:-1] == word_means:
                 time_segment.append(convert_to_time(array_line[0], array_line[1]))
                 # time_segment.append(lines[lines.index(line) + 1][0:-1])
-
     return time_segment
 
 
-def extract_audio_segment(path, time, name_audio):
-    newAudio = AudioSegment.from_wav(path)
-    newAudio = newAudio[time[0]:time[1]]
-    newAudio.export(name_audio + '.wav', format="wav") 
-
+def extract_audio_segment():
+    list_path = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+    for path in list_path:
+        list_segment_cho = get_time_segment('datan/' + path + '.srt', 'cho')
+        for _ in list_segment_cho:
+            newAudio = AudioSegment.from_wav('datan/' + path + '.wav')
+            newAudio = newAudio[_[0]:_[1]]
+            newAudio.export('data_export/' + path + '_' + str(random.randint(0, 999999)) + '.wav', format="wav")
 
 if __name__ == '__main__':
-    # list_path = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
-    # for path in list_path:
-    #     list_segment_cho = get_time_segment('datan/' + path + '.srt', 'cho')
-    #     for _ in list_segment_cho:
-    #         extract_audio_segment('datan/' + path + '.wav', _, 'data_export/' + path + '_' + str(random.randint(0, 999999)))    
-
+    
     (rate, sig) = wav.read('data_export/01_925408.wav')
     mfcc_feat = mfcc(sig,rate)
     fbank_feat = logfbank(sig,rate)
